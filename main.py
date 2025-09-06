63b200fef920f5c708c0c9daf113c20de1dee34c
sfhoffmann <sean.f.hoffmann@gmail.com>
2025-09-06 00:06:02 -0700

    add option to sort alphabetically

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import sys
+import argparse
 from stats import word_count, char_count, char_sort
 
 
 def main():
-    book_text = get_book_text(sys.argv[1])
+    parser = argparse.ArgumentParser(description="Count characters in a file.")
+    parser.add_argument("filename", help="Path to the file to analyze")
+    parser.add_argument("--sort", "-s",
+                        choices=["count", "alphabetical"],
+                        default="count",
+                        help="Sort results by count (default) or alphabetial")
+    
+    args = parser.parse_args()
+    book_text = get_book_text(args.filename)
     count = char_count(book_text)
-    sorted = char_sort(count)
+    sorted = char_sort(count, args.sort)
 
     print("============ BOOKBOT ============")
     print("Analyzing book found at books/frankenstein.txt...")
@@ -24,5 +33,5 @@ def get_book_text(book_file_path):
 try:   
     main()
 except Exception as e:
-    print("Usage: python3 main.py <path_to_book>")
+    print("Usage: python3 main.py <path_to_book> <--sort/-s count/alphabetical>")
     sys.exit(1)
--- stats.py
+++ stats.py
@@ -11,14 +11,23 @@ def char_count(book_text):
             chars[char] = 1
     return chars
 
-def char_sort(char_dict):
+def char_sort(char_dict, sort_by):
     sorted_chars = []
     for char in char_dict:
         sorted_chars.append(
             {"char": char, "num": char_dict[char]}
         )
-    sorted_chars.sort(reverse=True, key=sort_on)
+    sort_key = gen_sort_key(sort_by)
+    if sort_by == "alphabetical":
+        sorted_chars.sort(key=sort_key)
+    else:
+        sorted_chars.sort(reverse=True, key=sort_key)
     return sorted_chars
 
-def sort_on(chars):
-    return chars["num"]
+def gen_sort_key(sort_param):
+    def sort_on(chars):
+        if sort_param == "count":
+            return chars["num"]
+        elif sort_param == "alphabetical":
+            return chars["char"]
+    return sort_on
